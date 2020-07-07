3e5fddc60017d533a688d93660017ccb66068603
juliancesaro <julianjcesaro@gmail.com>
2020-07-07 21:53:24 +1000

    Created grid

---
+++ src/components/cell/Cell.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./Cell.css"
+
+const Cell = () => {
+  return <div className="cell"></div>
+}
+
+export default Cell
---
+++ src/components/grid/Grid.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./Grid.css"
+import Cell from "../cell/Cell"
+
+const Grid = ({ cols, rows }) => {
+  return (
+    <div
+      className="grid"
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${cols.length},  20px)`,
+      }}
+    >
+      {cols.map((col, i) => rows.map((row, k) => <Cell key={`${i}-${k}`} />))}
+    </div>
+  )
+}
+
+export default Grid
