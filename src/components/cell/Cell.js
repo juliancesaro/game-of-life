f5255fd3a1e2acd2c05b5234334f496893d8ca64
juliancesaro <julianjcesaro@gmail.com>
2020-07-09 16:40:44 +1000

    Added functionality to grid

--- src/components/cell/Cell.js
+++ src/components/cell/Cell.js
@@ -1,8 +1,21 @@
-import React from "react"
-import "./Cell.css"
+import React from "react";
+import "./Cell.css";
+import produce from "immer";
 
-const Cell = () => {
-  return <div className="cell"></div>
-}
+const Cell = ({ row, col, grid, setGrid }) => {
+  const toggleSelect = () => {
+    const newGrid = produce(grid, (gridCopy) => {
+      gridCopy[row][col] = 1;
+    });
+    setGrid(newGrid);
+  };
 
-export default Cell
+  return (
+    <div
+      className={`cell ${grid[row][col] ? "selected" : "notSelected"}`}
+      onClick={toggleSelect}
+    ></div>
+  );
+};
+
+export default Cell;
--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -1,19 +1,31 @@
-import React from "react"
-import "./Grid.css"
-import Cell from "../cell/Cell"
+import React, { useState } from "react";
+import "./Grid.css";
+import Cell from "../cell/Cell";
 
-const Grid = ({ cols, rows }) => {
+const Grid = ({ grid, setGrid, numRows, numCols, isActive }) => {
   return (
-    <div
-      className="grid"
-      style={{
-        display: "grid",
-        gridTemplateColumns: `repeat(${cols.length},  20px)`,
-      }}
-    >
-      {cols.map((col, i) => rows.map((row, k) => <Cell key={`${i}-${k}`} />))}
+    <div className="grid-wrapper">
+      <div
+        className="grid"
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols},  20px)`,
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <Cell
+              key={`${i}-${k}`}
+              row={k}
+              col={i}
+              grid={grid}
+              setGrid={setGrid}
+            />
+          ))
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Grid
+export default Grid;
